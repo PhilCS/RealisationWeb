6091b68b6d7395b5b18ff7f45db0539090212c8c
trepvane <trepvane@7a0ed0c6-9a36-40dd-914c-aba2faf934ba>
2013-09-24 04:14:24 +0000

    début de tests d'interface

---
+++ projet-mozambique/projet-mozambique/Controllers/PublicController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projet_mozambique.Controllers
+{
+    public class PublicController : Controller
+    {
+        //
+        // GET: /Public/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ projet-mozambique/projet-mozambique/Controllers/SectorielController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace projet_mozambique.Controllers
+{
+    public class SectorielController : Controller
+    {
+        //
+        // GET: /Sectoriel/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
