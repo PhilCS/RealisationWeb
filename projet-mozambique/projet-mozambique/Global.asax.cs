cf5504753731e2021c230287c862f163c1c65408
mnoreau <mnoreau@7a0ed0c6-9a36-40dd-914c-aba2faf934ba>
2013-10-27 20:41:26 +0000

    global.asax

--- projet-mozambique/projet-mozambique/Global.asax.cs
+++ projet-mozambique/projet-mozambique/Global.asax.cs
@@ -7,6 +7,9 @@ using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using projet_mozambique.Utilitaires;
+using WebMatrix.WebData;
+using WebMatrix.Data;
+using System.Globalization;
 
 namespace projet_mozambique
 {
@@ -23,6 +26,39 @@ namespace projet_mozambique
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            WebSecurity.InitializeDatabaseConnection("DefaultConnection", "UTILISATEUR", "ID", "NOMUTIL", true);
+
+        }
+
+        protected void Application_AcquireRequestState(object sender, EventArgs e)
+        {
+            //It's important to check whether session object is ready
+            if (HttpContext.Current.Session != null)
+            {
+                CultureInfo ci = (CultureInfo)this.Session["Culture"];
+                //Checking first if there is no value in session 
+                //and set default language 
+                //this can happen for first user's request
+                if (ci == null)
+                {
+                    //Sets default culture to english invariant
+                    string langName = "en";
+
+                    //Try to get values from Accept lang HTTP header
+                    if (HttpContext.Current.Request.UserLanguages != null &&
+                    HttpContext.Current.Request.UserLanguages.Length != 0)
+                    {
+                        //Gets accepted list 
+                        langName = HttpContext.Current.Request.UserLanguages[0].Substring(0, 2);
+                    }
+                    ci = new CultureInfo(langName);
+                    this.Session["Culture"] = ci;
+                }
+                //Finally setting culture for each request
+                System.Threading.Thread.CurrentThread.CurrentUICulture = ci;
+                System.Threading.Thread.CurrentThread.CurrentCulture = CultureInfo.CreateSpecificCulture(ci.Name);
+            }
         }
 
         protected void Application_Error(object sender, EventArgs e)
