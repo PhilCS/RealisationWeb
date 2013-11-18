de9d6431d0449f7818c890d37f26e1b201702eec
trepvane <trepvane@7a0ed0c6-9a36-40dd-914c-aba2faf934ba>
2013-11-18 19:09:16 +0000

    affichage des fils de discussion d'un forum (vue sommaire des fils et vue détaillée d'un fil)

---
+++ projet-mozambique/projet-mozambique/Models/FilModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using projet_mozambique.Utilitaires;
+
+namespace projet_mozambique.Models
+{
+    public class FilModel
+    {
+        public int id { get; set; }
+        public string sujet { get; set; }
+        public ListePaginee<MessageForumModel> listeMessages { get; set; }
+        public int nbMessages { get; set; }
+        public int nbLectures { get; set; }
+        public string dernierParticipant { get; set; }
+        public DateTime dateDerniereReponse { get; set; }
+    }
+}
