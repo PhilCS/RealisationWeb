12f9fa6932f8c9549a03a62f4535677c9368f8de
PhilCS <philippecsavard@gmail.com>
2013-11-25 13:32:05 +0000

    Correction erreur projet

---
+++ projet-mozambique/projet-mozambique/Models/dbContext.Designer.cs
@@ -0,0 +1,4 @@
+﻿// La génération de code par défaut est désactivée pour le modèle 'C:\Users\Philippe\Cégep\Réalisation Mozambique\equipe2\projet-mozambique\projet-mozambique\Models\dbContext.edmx'. 
+// Pour l'activer, modifiez la valeur du concepteur 'Stratégie de génération de code'.
+// Cette propriété est disponible dans la fenêtre Propriétés, lorsque le modèle est ouvert
+// dans le concepteur.
